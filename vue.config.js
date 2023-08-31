50195a7a5ada51bd57b2b1f7241990de0c143f67
ReadySetDice <info@readysetdice.com>
2023-08-31 16:22:43 +0200

    Added path require to vue config[C

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+const path = require('path')
 module.exports = {
   transpileDependencies: [
     'vuetify'
